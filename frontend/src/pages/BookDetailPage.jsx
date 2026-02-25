5f9b43cac7564220a44ffa5e0e7ea139e5c3de20
chmcs2026sohamshelke-rgb <chmcs2026.soham.shelke@gmail.com>
2026-02-25 09:45:38 +0530

    Update BookDetailPage.jsx

--- frontend/src/pages/BookDetailPage.jsx
+++ frontend/src/pages/BookDetailPage.jsx
@@ -1,11 +1,139 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
+import { LoaderIcon, Trash2Icon, ArrowLeftIcon } from "lucide-react";
 const BookDetailPage = () => {
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+  const { id } = useParams();
+  useEffect(() => {
+
+    const fetchBook = async () => {
+      try {
+        const res = await api.get(`/books/${id}`);
+        setBook(res.data);
+      } catch (error) {
+        console.error("Error fetching book", error);
+        toast.error("failed to fetch the book");
+      } finally {
+        setLoading(false);
+
+      }
+    };
+    fetchBook();
+  }, [id]);
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this book?"))
+      return;
+
+    try {
+      await api.delete(`/books/${id}`);
+      toast.success("Book Deleted Successfully");
+      navigate("/");
+
+    } catch (error) {
+      console.log("Error deleting book", error);
+      toast.error("failed to delete book");
+
+    }
+  };
+
+  const handleSave = async () => {
+    if (!book.title.trim() || !book.author.trim()) {
+      toast.error("please add title and author");
+      return;
+    }
+
+    setSaving(true);
+
+    try {
+      await api.put(`/books/${id}`, {
+        title: book.title,
+        author: book.author,
+        publishYear: Number(book.publishYear)
+      });
+
+      toast.success("Book updated successfully");
+      Navigate("/");
+
+    } catch (error) {
+      console.log("error updating book", error);
+      toast.error("failed to update book");
+    } finally {
+      setSaving(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className='min-h-screen bg-base-200 flex items-center justify-center'>
+
+        <LoaderIcon className='animate-spine size-10' />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      BookDetailPage
+    <div className='min-h-screen  bg-base-200'>
+      <div className='container mxx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+
+          {/*HEADER*/}
+
+          <div className="flex items-center justify-between mb-6">
+            <Link to="/" className="btn btn-ghost ">
+              <ArrowLeftIcon className="h-5 w-5" />Back to Books</Link>
+
+            <button onClick={handleDelete} className="btn btn-error btn-outline">
+              <Trash2Icon className="h-5 w-5" />Delete book
+            </button>
+          </div>
+
+          {/*FORM CARD*/}
+          <div className="card bg-base-100 shadow-lg">
+            <div className="card-body">
+              {/*Title*/}
+              <div className='form-control mb-4'>
+                <label className="label">
+                  <span className="label-text"></span>
+                </label>
+                <input type="text" placeholder="Book title" className="input input-bordered" value={book.title} onChange={(e) =>
+                  setBook({ ...book, title: e.target.value })
+                } />
+              </div>
+              {/*AUTHOR*/}
+              <div className='form-control mb-4'>
+                <label className='label'>
+                  <span className='label-text'>Author</span>
+                </label>
+                <input type="text" placeholder="Book author" className="input input-bordered" value={book.author} onChange={(e) => setBook({ ...book, author: e.target.value })} />
+              </div>
+
+              {/*PUBLISH YEAR*/}
+              <div className="form-control mb-6">
+                <label className="label">
+                  <span className="label-text">Publish Year</span>
+                </label>
+                <input type="text" placeholder="Year" className="input input-bordered" value={book.publishYear} onChange={(e) => setBook({ ...book, publishYear: e.target.value })} />
+
+              </div>
+
+              {/*ACTION*/}
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" disabled={saving}
+                  onClick={handleSave}>
+                  {saving ? "saving...." : "save changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default BookDetailPage
