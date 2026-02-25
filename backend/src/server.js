2613469247a09e4446b998a3326f2369890a2fef
chmcs2026sohamshelke-rgb <chmcs2026.soham.shelke@gmail.com>
2026-02-25 09:44:08 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import bookRoutes from "./routes/bookRoutes.js"
+import cors from "cors"
+
 dotenv.config()
 const app=express();
 const port=process.env.port ||3001
+app.use(cors())
 // app.get('/',(req,res)=>{
 //     res.status(200).json("Hello!")
 //     console.log('hello')
